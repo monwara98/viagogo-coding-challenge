9e14e5d4d6e9e1eaf4972fbd0995c4150ca5ab79
monwara98 <32258645+monwara98@users.noreply.github.com>
2017-11-03 12:14:14 +0000

    Update Coordinate.h

--- Coordinate.h
+++ Coordinate.h
@@ -57,43 +57,4 @@ public:
         return os << "(" << obj.xCoord << "," << obj.yCoord << ")";
 }
 
-
-
-class Event {
-
-public:
-    int id = 0;
-    int tickets = 0;
-    int price = 0;
-    Coordinate coord;
-
-    Event(int uniqueID, int t, int dollars, Coordinate c)
-      : id(uniqueID),
-        tickets(t),
-        price(dollars),
-        coord(c) {
-    }
-
-    int getID() {
-      return id;
-    }
-
-    int getX() {
-      return coord.getX();
-    }
-
-    int getY() {
-      return coord.getY();
-    }
-
-    int manhattanDistance(int x, int y) {
-      int distance = fabs(coord.checkX(coord.getX()) - x) + fabs(coord.checkY(coord.getY()) - y);
-      return distance;
-    }
-};
-
-    std::ostream& operator<<(std::ostream& os, const Event& obj) {
-        return os << "Event " << obj.id << " - " << "$" << obj.price;
-    }
-
 #endif
